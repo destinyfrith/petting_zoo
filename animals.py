a69024bd515c9986315c2c6d6fee5a0e42893081
Destiny Frith <desilyvahn@gmail.com>
2022-04-19 15:34:40 -0500

    added methods and converted objects to strings

--- animals.py
+++ animals.py
@@ -2,45 +2,48 @@ from models.glass_tank import Lizard, Snake, Worm, Eel, Snail
 from models.pond import Otter, Whale, Jellyfish, Dolphin, Penguin
 from models.petting_area import Llama, Leopard, Bear, Giraffe, RedPanda
 
-miss_fuzz = Llama("Miss Fuzz", "domestic llama")
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "llama chow")
 
 print(miss_fuzz.name)
 
-lenny = Leopard("Lenny", "clouded")
+lenny = Leopard("Lenny", "clouded", "morning", "rabbit")
 
 print(lenny.name)
 
-barry = Bear("Barry", "black")
+barry = Bear("Barry", "black", "midday", "salmon")
 
 print(barry.name)
 
-gerald = Giraffe("Gerald", "african")
+gerald = Giraffe("Gerald", "african", "afternoon", "leaves")
 
 print(gerald.name)
 
-randy = RedPanda("Randy", "endangered")
+randy = RedPanda("Randy", "endangered", "afternoon", "berries")
 
 print(randy.name)
 
-ollie = Otter("Ollie", "north american river")
+ollie = Otter("Ollie", "north american river", "fish")
 
 print(ollie.name)
 
-wendy = Whale("Wendy", "humpback")
+wendy = Whale("Wendy", "humpback", "tuna")
 
-jake = Jellyfish("Jake", "upside down")
+jake = Jellyfish("Jake", "upside down", "jellyfish food")
 
-devin = Dolphin("Devin", "spinner")
+devin = Dolphin("Devin", "spinner", "chocolate")
 
-patrick = Penguin("Patrick", "macaroni")
+patrick = Penguin("Patrick", "macaroni", "mackerel")
 
-leroy = Lizard("Leroy", "gecko")
+leroy = Lizard("Leroy", "gecko", "flies")
 
-samuel = Snake("Samuel", "ball python")
+samuel = Snake("Samuel", "ball python", "rat")
 
-william = Worm("William", "roundworm")
+william = Worm("William", "roundworm", "leaves")
 
-ellie = Eel("Ellie", "snowflake")
+ellie = Eel("Ellie", "snowflake", "insects")
 
-stan = Snail("Stan", "mediterranean green")
+stan = Snail("Stan", "mediterranean green", "mushrooms")
 
+print(f'{lenny.name} the {lenny.species} is available to pet during the {lenny.shift} shift.')
+
+print(miss_fuzz)
--- models/petting_area/walking_classes.py
+++ models/petting_area/walking_classes.py
@@ -4,54 +4,94 @@ from datetime import date
 
 class Llama:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 
 class Leopard:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 
 class Bear:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 
 class Giraffe:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 
 class RedPanda:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
+        self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
